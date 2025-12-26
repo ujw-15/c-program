90317583544b42ebf798d68607e21bcb26ff47b7
uw15-boop <umsunggyu@gmail.com>
2025-12-26 21:44:49 +0900

    (2025,12,26) 문자열

--- Project1/source.c
+++ Project1/source.c
@@ -18,10 +18,12 @@ void main()
 //
 //	// 배열의 메모리 공간은 프로그램이 실행되는 동안
 //	// 변경할 수 없습니다.
-	
+//
 //	int array[ ] = { 1, 2, 3, 4, 5 };
 //	
 //	int size = sizeof(array) / sizeof(array[0]);
+// 
+// 	   int* ptr = &array;
 //
 //	for (int i = 0; i < size; i++)
 //	{
@@ -30,16 +32,91 @@ void main()
 //
 //	// 배열의 크기는 생략할 수 있으며, 초기화 목록에서
 //	// 설정한 요소에 따라 배열의 크기가 결정됩니다.
+//
+//	printf("배열의 시작 주소 : %p\n", array);
+//
+//	ptr = ptr + 1;
+//
+//	*ptr = 22;
+//
+//	printf("ptr 변수의 값 : %p\n", ptr);
+//	printf("array[1]의 값 : %d\n", array[1]);
+//
+//	// 배열의 연속적인 메모리 공간을 가지며, 배열의 이름은
+//	// 배열의 시작 주소를 가리킵니다.
 
-	int room[] = { 10, 20, 30, 40, 50 };
-
-	int* ptr = &room;
-
-	ptr + 1 ;
-
-	*ptr = 20;
-	printf("%d\n", *ptr);
 
 #pragma endregion
+
+#pragma region 문자열
+//	// 연속적인 메모리 공간에 저장된 문자 변수의
+//	// 집합입니다.
+//
+//	const char* character = "world";
+//
+//	
+//
+//	printf("character가 가리키는 문자열 : %s\n", character);
+//
+//	char message[] = "message";
+//
+//	*(message + 1)/* message[1] */ = 'A';
+//
+//	printf("message가 가리키는 문자열 %s\n", message);
+//
+//
+//	// 문자열의 경우 포인터를 이용하여 문자열 상수를 가리키도록 할
+//	// 수 있으며, 문자열 상수는 데이터 영역의 읽기 전용 공간에 저장
+//	// 되기 떄문에 문자열의 값을 변경할 수 없습니다.
+//
+//	character = "exercise";
+//
+//	printf("character가 가리키는 문자열 : %s\n", character);
+//
+//	// 문자열은 공백도 함께 메모리 공간에 포함하여 크기가 결정되며,
+//	// 마지막에 문자열의 끝을 알려주는 제어 문자가 추가됩니다.
+//
+//	character = "show\0er";
+//
+//	printf("character가 가리키는 문자열 : %s\n", character);
+//
+//	// 문자열의 경우 서로 연속적인 메모리 공간으로 연결되어 있지만,
+//	// 문자 배열 사이에 무효의 문자를 넣게 되면 무효의 문자까지만 출력됩니다.
+#pragma endregion
+
+#pragma region  (2)차원 배열
+//	// 배열의 요소로 또 다른 배열을 가지는 배열입니다.
+//
+//	int array2D[3][3] =
+//	{
+//		{1, 2, 3},
+//		{4, 5, 6},
+//		{7, 8, 9},
+//	};
+//	for (int i = 0; i < 3; i++) {
+//		for (int k = 0; k < 3; k++) {
+//			printf("%d,", array2D[i][k]);
+//		}
+//		printf("\n");
+//	}
+//	
+//	
+//	// 2차원 배열은 행과 열로 구분되며, 앞에 있는 배열은
+//	// 열을 의미하고, 뒤에 있는 배열은 행을 의미합니다.
+#pragma endregion
+
+#pragma region  아스키 코드
+//// 미국 국립 표준 협회에서 표준화한 정보 교환용
+//// 7 bit 부호 체계입니다.
+//
+//char alphabet = 'A';
+//
+//printf("alphabet 변수의 10 진수 값 : %d\n", alphabet);
+//
+//for (int a = 0; a < 26; a++) {
+//	printf("%c ",'A' + a);
+//}
+#pragma endregion
+
 
 }
